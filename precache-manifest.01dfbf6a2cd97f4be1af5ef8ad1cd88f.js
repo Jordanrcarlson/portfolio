eade27c0421bedfaf9b86f2d2cd6701a5439a67c
Jordan <jordanrcarlson@gmail.com>
2020-04-27 10:54:52 -0700

    Updates

---
+++ precache-manifest.01dfbf6a2cd97f4be1af5ef8ad1cd88f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8072b49c01d3be524ad63725f9b7d2ef",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "71c6db9c5d0a5d0143e2",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5f353b10e578a733f2b9",
+    "url": "/portfolio/static/js/2.ff8bd651.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ff8bd651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c6db9c5d0a5d0143e2",
+    "url": "/portfolio/static/js/main.82aa3a4d.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  }
+]);
