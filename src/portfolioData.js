921adfcb88c9df9a25e2d8a6ac3fbdb4aa3eda50
Jordan <jordanrcarlson@gmail.com>
2020-04-27 17:20:06 -0700

    website

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -1,6 +1,6 @@
 let portfolioData = {
- "imagebaseurl":"https://Jordancarlson.github.io",
-//   "imagebaseurl":"http://localhost:3000/",
+ "imagebaseurl":"ssh://git@github.com:Jordancarlson.github.io.git",
+  // "imagebaseurl":"http://localhost:3000/",
   "name": "Jordan Carlson",
   "role": "Software Developer and Electrical Engineer"
 }
