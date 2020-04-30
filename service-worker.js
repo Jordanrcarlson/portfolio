844fe329c71bca9372eae87cd775d26943712991
Jordan <jordanrcarlson@gmail.com>
2020-04-30 16:06:32 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c7fc956b9c7b987ba96545ce97193850.js"
+  "/portfolio/precache-manifest.19b74f7a3cc9f5ec96599c102be6846e.js"
 );
 
 self.addEventListener('message', (event) => {
