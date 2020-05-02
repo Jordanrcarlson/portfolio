faf98a51ad563d92077c17297efc2429cc8be92e
Jordan <jordanrcarlson@gmail.com>
2020-05-02 12:21:56 -0700

    Updates

--- js/init.js
+++ js/init.js
@@ -153,8 +153,24 @@
 
         type: "POST",
 	      url: "https://formspree.io/xbjzjjjy",
-	      data: data
+	      data: data,
+	      success: function(msg) {
 
+            // Message was sent
+            if (msg == 'OK') {
+               $('#image-loader').fadeOut();
+               $('#message-warning').hide();
+               $('#contactForm').fadeOut();
+               $('#message-success').fadeIn();
+            }
+            // There was an error
+            else {
+               $('#image-loader').fadeOut();
+               $('#message-warning').html(msg);
+	            $('#message-warning').fadeIn();
+            }
+
+	      }
 
       });
       return false;
