46e032d5b1ad011f0edc90ccc01dc32e428fb513
Jordan Carlson <jordanrcarlson@gmail.com>
2025-01-27 23:13:40 -0800

    Updates About Me and Resumes

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,17 +9,19 @@ export default class Header extends Component {
               <img className="profile-pic" src="images/profilepic.jpg" alt="" />
             </div>
             <div className="nine columns main-col">
-              <h2>About Me 1</h2>
+              <h2>About Me</h2>
               <p> I am graduated from the electrical engineering program at UVic. 
                   I am seeking employment to complete my fourth year as a professional developer.</p>
-              <p>  I have experience particularly with EDAs and CAD, Python, C and C++, Javascript, Java, React, HTML, CSS, 
-                   and many frameworks within these universes. I enjoy developing engineering solutions
-                   and improving usability and interfaces, employing usability heuristics while sustaining coding practice 
-                   excellence and sustaining transparent, effective team communications and integration.
+              <p>  I am currently particularly excited about AI tools increasing in ease and speed of development. 
+                   It has been stimulating integrating them with my experience with EDAs and CAD, Python, 
+                   C and C++, Javascript, Java, React, HTML, CSS, and within the many frameworks within these universes. 
+                   I enjoy developing engineering solutions, improving usability and interfaces, employing usability 
+                   heuristics in parallel, while sustaining coding practice excellence and sustaining transparent, 
+                   effective team communications and integration.
               </p>
-              <p> I'm searching for a team to grow with, and to develop my skillset and portfolio.
-              I have four years of experience in challenging leadership positions prior to working in tech, 3 
-              years of experience as a junior software developer, and I hope for another demanding and impactful role </p>
+              <p> I'm searching for a team to build and grow with.
+              I have four years of experience in challenging leadership positions prior to working in tech. Since then, I've 
+              accumulated 3 years of experience as a junior software developer, and I hope for another demanding and impactful role. </p>
               <div className="row">
                 <div className="columns contact-details">
                   <h2>Contact Details</h2>
@@ -34,8 +36,11 @@ export default class Header extends Component {
                 </div>
                 <div className="columns download">
                   <p>
-                    <a href="images/resume.pdf" className="button"><i className="fa fa-download" />Download Resume</a>
-                    <a href="images/coverletter.pdf" className="button"><i className="fa fa-download" />Download Cover Letter</a>
+                    <a href="images/software_resume.pdf" className="button"><i className="fa fa-download" /> Download Resume (Software)   </a>
+                    <a href="images/electronics_resume.pdf" className="button"><i className="fa fa-download" /> Download Resume (Electronics)   </a>
+                    <a href="images/electrical_resume.pdf" className="button"><i className="fa fa-download" /> Download Resume (Electrical)   </a>
+                    <a href="images/manager_resume.pdf" className="button"><i className="fa fa-download" /> Download Resume (Engineering Manager)   </a>
+                    <a href="images/coverletter.pdf" className="button"><i className="fa fa-download" />Download Cover Letter (Generic)</a>
                   </p>
                 </div>
               </div> {/* end row */}
